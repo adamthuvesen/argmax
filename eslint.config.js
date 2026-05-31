f0de0030cb080a4fbf613de62cc1cdf61e7aa8c2
adamthuvesen <adam.thuvesen@mentimeter.com>
2026-05-31 21:42:14 +0200

    chore(eslint): drop reference to removed tsconfig.main.json

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,7 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.json", "./tsconfig.main.json"],
+        project: ["./tsconfig.json"],
         tsconfigRootDir: import.meta.dirname
       }
     },
